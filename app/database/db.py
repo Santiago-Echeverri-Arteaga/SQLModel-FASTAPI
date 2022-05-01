d3158c2b3df1b8cf4340c7f33edc2cbda7ebefe8
Santiago Echeverri Arteaga <81872550+Santiago-Echeverri-Arteaga@users.noreply.github.com>
2022-05-01 18:40:40 -0500

    Update db.py

--- app/database/db.py
+++ app/database/db.py
@@ -2,8 +2,8 @@ import boto3
 
 dynamodbTableName = "heroes-db"
 dynamodb = boto3.resource('dynamodb')#,
-#aws_access_key_id="AKIAU4ZGB244UHGG4K4V",
-#aws_secret_access_key="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",
+#aws_access_key_id="",
+#aws_secret_access_key="",
 #region_name="us-east-1")
-#Descomentar las líneas de arriba para deployment por fuera de AWS
+#Descomentar las líneas de arriba y poner los datos correspondientes para deployment por fuera de AWS
 heroes_table = dynamodb.Table(dynamodbTableName)
