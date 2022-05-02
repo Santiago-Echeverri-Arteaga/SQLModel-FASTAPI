2603884bcf51987f965dd74d27f97be5bd995f65
Santiago Echeverri Arteaga <secheverri@uqvirtual.edu.co>
2022-05-02 08:07:12 -0500

    uuid

--- app/api.py
+++ app/api.py
@@ -6,6 +6,7 @@ from mangum import Mangum
 # app.include_router(api_router, prefix=config.settings.prefix) guardado en
 # el config file
 app = FastAPI()
+#Incluir los endpoints
 ##app.include_router(heroes.router, prefix="/heroes",tags=["Heroes"])
 
 @app.get("/")
